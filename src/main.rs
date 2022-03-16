1c95d786a70384e0594b20e693b3b0352539fcfe
sushij <sushant.wurk@gmail.com>
2022-03-17 01:23:13 +0530

    █

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
+// @prettier-ignore
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
     let (mut socket, _addr) = listener.accept().await.unwrap();
-    let mut buffer = [0u8; 1024];
 
-    let bytes_read = socket.read(&mut buffer).await.unwrap();
-    socket.write_all(&buffer[..bytes_read]).await.unwrap();
+    let (reader, mut writer) = socket.split();
 
-}   
+    let mut reader = BufReader::new(reader);
+    let mut line = String::new();
+    loop {
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        if bytes_read == 0 {
+            break;
+        }
+        writer.write_all(&line.as_bytes()).await.unwrap();
+        line.clear();
+    }
+}
