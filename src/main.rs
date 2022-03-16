b919e7dbf52c15391b3fbb9d606d4913e2ab2eb8
sushij <sushant.wurk@gmail.com>
2022-03-17 00:15:55 +0530

    ß

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
-}
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+    let mut buffer = [0u8; 1024];
+
+    let bytes_read = socket.read(&mut buffer).await.unwrap();
+    socket.write_all(&buffer[..bytes_read]).await.unwrap();
+
+}   
