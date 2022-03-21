32613e583b1a9b415efddec2b7f33e52c1ccbb72
sushij <sushant.wurk@gmail.com>
2022-03-21 22:50:09 +0530

    Donzo

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,19 @@ use tokio::sync::broadcast;
 async fn main() {
     // Initiate listener
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
-    //Connect Multiple Clients
-    let (tx, _rx) = broadcast::channel::<String>(10);
+
+    // Create a channel
+    let (tx, _rx) = broadcast::channel(10);
     loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        // Sender, Cloned cause of moved error
         let tx = tx.clone();
         let mut rx = tx.subscribe();
         // async block
         tokio::spawn(async move {
+            //Split the socket to get reader and writer
             let (reader, mut writer) = socket.split();
-
+            // creating a new bufReader
             let mut reader = BufReader::new(reader);
             let mut line = String::new();
 
@@ -25,12 +28,14 @@ async fn main() {
                         if result.unwrap() == 0 {
                             break;
                         }
-                        tx.send(line.clone()).unwrap();
+                        tx.send((line.clone(), addr)).unwrap();
                         line.clear();
                     }
-                    result = rx.recv() =>{
-                        let msg = result.unwrap();
-                        writer.write_all(&msg.as_bytes()).await.unwrap();
+                    result = rx.recv() => {
+                        let (msg, oth_addr) = result.unwrap();
+                        if addr != oth_addr {
+                            writer.write_all(&msg.as_bytes()).await.unwrap();
+                        }
                     }
                 }
             }
